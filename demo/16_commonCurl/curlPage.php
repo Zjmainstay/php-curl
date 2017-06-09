74c4559e43ca423b185406f8f687e3a64eecc9c5
Zjmainstay <hzgdys@163.com>
2017-06-09 09:56:50 +0800

    为通用方法添加opt追加功能

--- demo/16_commonCurl/curlPage.php
+++ demo/16_commonCurl/curlPage.php
@@ -8,12 +8,13 @@
  * @param  string $url      采集的url
  * @param  array $postData  post提交的数据
  * @param  array $header    header头数组，curl命令里的-H参数组成的数组
+ * @param  array $opts      option数组，供额外添加opt属性，key为curl_setopt第二参数，value为curl_setopt第三参数
  * @param  string $cookieSaveFile    存储cookie的文件
  * @param  string $cookieGetFile     读取cookie的文件（可以是上一个getPage存下来的cookie文件）
  * @param  string $timeout 超时时间
  * @return string
  */
-function curlPage( $url, $postData = array(), $header = array(), $cookieSaveFile = '', $cookieGetFile = '' , $timeout = 60 ) {
+function curlPage( $url, $postData = array(), $header = array(), $opts = array(), $cookieSaveFile = '', $cookieGetFile = '' , $timeout = 60 ) {
     $ch  = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);       //返回数据不直接输出
     curl_setopt($ch, CURLOPT_ENCODING, "gzip");        //指定gzip压缩
@@ -45,6 +46,14 @@ function curlPage( $url, $postData = array(), $header = array(), $cookieSaveFile
     if(!empty($header)) {
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);      //使用header头信息
     }
+
+    //额外option
+    if(!empty($opts)) {
+        foreach($opts as $key => $value) {
+            curl_setopt($ch, $key, $value);      //使用header头信息
+        }
+    }
+
     //存储cookie到文件
     if(!empty($cookieSaveFile)) {
         curl_setopt($ch,CURLOPT_COOKIEJAR,$cookieSaveFile); //存储提交后得到的cookie数据
@@ -95,7 +104,7 @@ echo "referer in header ok: ", preg_replace('#\s+#', '', substr(curlPage($url, $
 
 #测试超时
 $url = 'http://demo.zjmainstay.cn/php/curl/sleep3seconds.php';
-echo "timeout in 1's fail: ", substr(curlPage($url, $post, $header, false, false, 1), 0, 100) , "\n";
+echo "timeout in 1's fail: ", substr(curlPage($url, $post, $header, array(), false, false, 1), 0, 100) , "\n";
 
 #测试gzip正常
 $url = 'http://news.sohu.com/';
@@ -108,7 +117,8 @@ echo "301 is ok: ", substr(curlPage($url), 0, 100) , "\n";
 #测试存储cookie成功
 $url = 'http://demo.zjmainstay.cn/jquery/autoCheckCaptcha/createcode.php?t=' . rand(100000, 999999);
 $saveCookieFile = __DIR__ . '/curlPage.cookie.txt';
-$imgContent = curlPage($url, false, false, $saveCookieFile);
+if(file_exists($saveCookieFile)) { @unlink($saveCookieFile); }
+$imgContent = curlPage($url, false, false, array(), $saveCookieFile);
 $codeFile = __DIR__ . '/code.png';
 file_put_contents($codeFile, $imgContent);
 echo "save cookie is ok: ", file_get_contents($saveCookieFile) , "\n";
@@ -121,4 +131,12 @@ $url = 'http://demo.zjmainstay.cn/jquery/autoCheckCaptcha/checkcode.php';
 $post = array(
               'code' => $code,    //手动填写验证码并提交运行
               );
-echo "use cookie is ok: ", curlPage($url, $post, false, false, $saveCookieFile) , "\n";
+echo "use cookie is ok when status=1: ", curlPage($url, $post, false, array(), false, $saveCookieFile) , "\n";
+
+#测试添加额外opt
+$url = 'http://zjmainstay.cn';
+$content = curlPage($url, array(), false, array(
+                                        CURLOPT_FOLLOWLOCATION => FALSE,
+                                        CURLOPT_HEADER => TRUE,
+                                        ));
+echo "get url with no follow(302) and get header info: \n" , substr($content, 0, 200), "\n";
