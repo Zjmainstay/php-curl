2a6f5a22546e79f4e1535082d170dd9dceb53edb
Zjmainstay <hzgdys@163.com>
2018-04-29 14:06:05 +0800

    图床图片上传接口

--- demo/38_upload.otar.im_image_upload.php
+++ demo/38_upload.otar.im_image_upload.php
@@ -5,8 +5,6 @@
  * @author Zjmainstay
  * @website http://www.zjmainstay.cn
  * @project https://github.com/Zjmainstay/php-curl
- * 
- * 访问登录页，提交登录表单，查看登录结果
  */
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -32,6 +30,7 @@ $contentLength = strlen($data);
 // $apiUrl = 'http://upload.otar.im/api/upload/smms';
 $apiUrl = 'http://upload.otar.im/api/upload/imgur';
 
+//2. 上传图片
 $curl = "curl '{$apiUrl}' -H 'Accept: */*' --compressed -H 'Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2' -H 'Connection: keep-alive' -H 'Cookie: koa.sid=6fuuKG4qGhJg2CFP0Y3Go1X92QKNjlI2; koa.sid.sig=FqGx8un0r_LBzf_60ROuaik-pPE' -H 'Host: upload.otar.im' -H 'Referer: http://upload.otar.im/' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:59.0) Gecko/20100101 Firefox/59.0' -H 'x-csrf-token: {$csrfToken}' -H 'Content-Type: {$contentType}' -H 'Content-Length: {$contentLength}'";
 // die(var_dump($curl));
 $content = $autologin->execCurl($curl, function($parseCurlResult) use ($data) {
